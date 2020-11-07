f6c665b532737ca7c08166004e505f1d8a9c1423
Werner Gehrke Junior <werner.junior@outlook.com>
2020-11-07 16:20:51 -0300

    criação da migration e model da tabela de usuários

--- src/database/migrations/1604775942051-CreateUsers.ts
+++ src/database/migrations/1604775942051-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1604773234718
-  implements MigrationInterface {
+export default class CreateUsers1604775942051 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,13 +14,19 @@ export default class CreateAppointments1604773234718
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
             isNullable: false,
           },
           {
@@ -40,6 +45,6 @@ export default class CreateAppointments1604773234718
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
