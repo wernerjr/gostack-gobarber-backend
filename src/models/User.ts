9fda2358d631d68ccae0cffc5802a4795b15227d
Werner Gehrke Junior <werner.junior@outlook.com>
2020-11-07 17:04:48 -0300

    Criação do service para cadastrar um novo usuário

--- src/models/User.ts
+++ src/models/User.ts
@@ -18,7 +18,7 @@ class User {
   email: string;
 
   @Column('varchar')
-  password: string;
+  password?: string;
 
   @CreateDateColumn()
   created_at: Date;
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { email, name, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      email,
+      name,
+      password,
+    });
+
+    delete user.password;
+
+    return response.status(201).json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
